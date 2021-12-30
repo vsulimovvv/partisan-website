d3c5116634d62175bf64af40a441ab1ddc099b79
vsulimovvv <super.sulimov2014@yandex.ru>
2021-12-30 15:57:05 +0300

    fix sliders

--- app/js/main.js
+++ app/js/main.js
@@ -103,28 +103,28 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   })();
 
-  const swiper2 = new Swiper(".slider-videos", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    breakpoints: {
-      320: {
-        spaceBetween: 20,
-        slidesPerView: 1.2,
-      },
-      576: {
-        slidesPerView: 2.2,
-        spaceBetween: 25,
-      },
-      900: {
-        slidesPerView: 2.2,
-        spaceBetween: 30,
-      },
-      991: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-      },
-    },
-  });
+  // const swiper2 = new Swiper(".slider-videos", {
+  //   slidesPerView: 3,
+  //   spaceBetween: 30,
+  //   breakpoints: {
+  //     320: {
+  //       spaceBetween: 20,
+  //       slidesPerView: 1.2,
+  //     },
+  //     576: {
+  //       slidesPerView: 2.2,
+  //       spaceBetween: 25,
+  //     },
+  //     900: {
+  //       slidesPerView: 2.2,
+  //       spaceBetween: 30,
+  //     },
+  //     991: {
+  //       slidesPerView: 3,
+  //       spaceBetween: 30,
+  //     },
+  //   },
+  // });
 
   // const swiper22 = new Swiper(".holidays-held__slider .slider", {
   //   slidesPerView: 'auto',
@@ -154,145 +154,147 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 
-  // // * ==== Dropdown
-  // document.addEventListener('click', e => {
-  //   const isDropdownButton = e.target.closest("[data-dropdown-button]")
-  //   if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return;
+  // * ==== Dropdown
+  document.addEventListener('click', e => {
+    const isDropdownButton = e.target.closest("[data-dropdown-button]")
+    if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return;
 
-  //   let currentDropdown;
-  //   if (isDropdownButton) {
-  //     currentDropdown = e.target.closest('[data-dropdown]');
-  //     currentDropdown.classList.toggle('active');
-  //   };
+    let currentDropdown;
+    if (isDropdownButton) {
+      currentDropdown = e.target.closest('[data-dropdown]');
+      currentDropdown.classList.toggle('active');
+    };
 
-  //   document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
-  //     if (dropdown === currentDropdown) return;
-  //     dropdown.classList.remove('active');
-  //   });
-  // });
+    document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
+      if (dropdown === currentDropdown) return;
+      dropdown.classList.remove('active');
+    });
+  });
 
 
-  // // * ==== Toggle Tabs
-  // function tabsSlider(
-  //   headerSelector,
-  //   tabSelector,
-  //   contentSelector,
-  //   activeClass
-  // ) {
-  //   const header = document.querySelectorAll(headerSelector);
-  //   const tab = document.querySelectorAll(tabSelector);
-  //   const content = document.querySelectorAll(contentSelector);
+  // * ==== Toggle Tabs
+  function tabsSlider(
+    headerSelector,
+    tabSelector,
+    contentSelector,
+    activeClass
+  ) {
+    const header = document.querySelectorAll(headerSelector);
+    const tab = document.querySelectorAll(tabSelector);
+    const content = document.querySelectorAll(contentSelector);
 
-  //   header.forEach(el => {
-  //     hideTabContent();
-  //     showTabContent();
+    header.forEach(el => {
+      hideTabContent();
+      showTabContent();
 
-  //     function hideTabContent() {
-  //       content.forEach((item) => {
-  //         item.classList.remove('active');
-  //       });
-  //       tab.forEach((item) => {
-  //         item.classList.remove(activeClass);
-  //       });
-  //     }
+      function hideTabContent() {
+        content.forEach((item) => {
+          item.classList.remove('active');
+        });
+        tab.forEach((item) => {
+          item.classList.remove(activeClass);
+        });
+      }
 
-  //     function showTabContent(i = 0) {
-  //       content[i].classList.add('active');
-  //       tab[i].classList.add(activeClass);
-  //     }
+      function showTabContent(i = 0) {
+        content[i].classList.add('active');
+        tab[i].classList.add(activeClass);
+      }
 
-  //     header.forEach((item) => {
-  //       if (item) {
-  //         item.addEventListener('click', (e) => {
-  //           const target = e.target;
+      header.forEach((item) => {
+        if (item) {
+          item.addEventListener('click', (e) => {
+            const target = e.target;
 
-  //           if (
-  //             target.classList.contains(tabSelector.replace(/\./, ''))
-  //           ) {
-  //             tab.forEach((item, i) => {
-  //               if (target == item || target.parentNode == item) {
-  //                 hideTabContent();
-  //                 showTabContent(i);
-  //               }
-  //             });
-  //           }
-  //         });
-  //       }
-  //     });
-  //   })
-  // };
-  // tabsSlider(
-  //   '.conference-hall__wrapper',
-  //   '.conference-hall__button',
-  //   '.conference-hall__content',
-  //   'conference-hall__button--active'
-  // );
+            if (
+              target.classList.contains(tabSelector.replace(/\./, ''))
+            ) {
+              tab.forEach((item, i) => {
+                if (target == item || target.parentNode == item) {
+                  hideTabContent();
+                  showTabContent(i);
+                }
+              });
+            }
+          });
+        }
+      });
+    })
+  };
+  tabsSlider(
+    '.conference-hall__wrapper',
+    '.conference-hall__button',
+    '.conference-hall__content',
+    'conference-hall__button--active'
+  );
 
-  // // * === add more holidays for you
-  // (function addHolidayCard() {
-  //   const listContainer = document.querySelector('.filters__items');
-  //   const addBtn = document.querySelector('.filters__btn');
+  // * === add more holidays for you
+  (function addHolidayCard() {
+    const listContainer = document.querySelector('.filters__items');
+    const addBtn = document.querySelector('.filters__btn');
 
-  //   const namesCard = ['Пакет "Максимум"', 'Пакет "Взрослый"', 'Пакет "Для девочек"', 'Пакет "Для мальчиков"'];
-  //   const priceCard = ['900', '300', '500', '490'];
-  //   const quantityPeople = ['4', '8', '5', '7'];
-  //   const pickerCard = ['Хит', 'Для мальчиков', 'Для девочек', 'Для взрослых', 'Новый', 'Акция'];
+    if (addBtn) {
+      const namesCard = ['Пакет "Максимум"', 'Пакет "Взрослый"', 'Пакет "Для девочек"', 'Пакет "Для мальчиков"'];
+      const priceCard = ['900', '300', '500', '490'];
+      const quantityPeople = ['4', '8', '5', '7'];
+      const pickerCard = ['Хит', 'Для мальчиков', 'Для девочек', 'Для взрослых', 'Новый', 'Акция'];
 
-  //   function rendomNames() {
-  //     return namesCard[Math.floor(Math.random() * namesCard.length)];
-  //   }
+      function rendomNames() {
+        return namesCard[Math.floor(Math.random() * namesCard.length)];
+      };
 
-  //   function rendomPrice() {
-  //     return priceCard[Math.floor(Math.random() * priceCard.length)];
-  //   }
+      function rendomPrice() {
+        return priceCard[Math.floor(Math.random() * priceCard.length)];
+      };
 
-  //   function rendomPeople() {
-  //     return quantityPeople[Math.floor(Math.random() * quantityPeople.length)];
-  //   }
+      function rendomPeople() {
+        return quantityPeople[Math.floor(Math.random() * quantityPeople.length)];
+      };
 
-  //   function rendomPricker() {
-  //     return pickerCard[Math.floor(Math.random() * pickerCard.length)];
-  //   }
+      function rendomPricker() {
+        return pickerCard[Math.floor(Math.random() * pickerCard.length)];
+      };
 
-  //   function generateCard() {
-  //     return ` 
-  //     <li class="filters__item mix girl">
-  //       <article class="filters-card">
-  //         <div class="filters-card__img">
-  //           <img src="images/content/entertainments/1.jpg" alt="">
-  //         </div>
-  //         <div class="filters-card__right">
-  //           <div class="filters-card__pickers">
-  //             <span class="filters-card__picker">${rendomPricker()}</span>
-  //           </div>
-  //           <div class="filters-card__content">
-  //             <div class="filters-card__details">
-  //               <h3 class="filters-card__name">${rendomNames()}</h3>
-  //               <div class="filters-card__quantity">от ${rendomPeople()} человек</div>
-  //               <div class="filters-card__price">от <span>${rendomPrice()} ₽</span> за чел. </div>
-  //               <button class="filters-card__btn btn btn--small">Подробнее</button>
-  //             </div>
-  //             <div class="filters-card__info">
-  //               <div class="filters-card__text">
-  //                 <p>Новые и необычные развлечения для дружных компаний-это то, что необходимо всем, независимо от
-  //                   возраста. Лазертаг-приключения смогут порадовать Вас играми на различный вкус. Полоса препятствий,
-  //                   любимая еще с детства компьютерная игра или страшные приведения? Всё это Вы сможете испытать на
-  //                   лазертаг-приключениях.</p>
-  //               </div>
-  //               <button class="filters-card__btn--more">Развернуть</button>
-  //               <button class="filters-card__btn btn">Хочу поиграть</button>
-  //             </div>
-  //           </div>
-  //         </div>
-  //       </article>
-  //     </li>
-  //     `;
-  //   };
+      function generateCard() {
+        return ` 
+      <li class="filters__item mix girl">
+        <article class="filters-card">
+          <div class="filters-card__img">
+            <img src="images/content/entertainments/1.jpg" alt="">
+          </div>
+          <div class="filters-card__right">
+            <div class="filters-card__pickers">
+              <span class="filters-card__picker">${rendomPricker()}</span>
+            </div>
+            <div class="filters-card__content">
+              <div class="filters-card__details">
+                <h3 class="filters-card__name">${rendomNames()}</h3>
+                <div class="filters-card__quantity">от ${rendomPeople()} человек</div>
+                <div class="filters-card__price">от <span>${rendomPrice()} ₽</span> за чел. </div>
+                <button class="filters-card__btn btn btn--small">Подробнее</button>
+              </div>
+              <div class="filters-card__info">
+                <div class="filters-card__text">
+                  <p>Новые и необычные развлечения для дружных компаний-это то, что необходимо всем, независимо от
+                    возраста. Лазертаг-приключения смогут порадовать Вас играми на различный вкус. Полоса препятствий,
+                    любимая еще с детства компьютерная игра или страшные приведения? Всё это Вы сможете испытать на
+                    лазертаг-приключениях.</p>
+                </div>
+                <button class="filters-card__btn--more">Развернуть</button>
+                <button class="filters-card__btn btn">Хочу поиграть</button>
+              </div>
+            </div>
+          </div>
+        </article>
+      </li>
+      `;
+      };
 
-  //   addBtn.addEventListener('click', e => {
-  //     listContainer.insertAdjacentHTML('beforeend', generateCard());
-  //   });
-  // })();
+      addBtn.addEventListener('click', e => {
+        listContainer.insertAdjacentHTML('beforeend', generateCard());
+      });
+    }
+  })();
 
   // // * === Show More
   // // function showMore() {
